ad4b77b520b473490a938f7d7431ed26458bac81
Samiul Hoque Chowdhury <samiulofficialworks@gmail.com>
2024-09-18 01:09:37 +0530

    two sum

---
+++ Array Leetcode/TwoSum.cpp
@@ -0,0 +1,35 @@
+/*
+Two Sum
+Problem: Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1] (because nums[0] + nums[1] = 9)
+
+
+*/
+
+/*
+
+++++++++++solution submitted on leetcode:+++++++++
+
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> num_map;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            
+            if (num_map.find(complement) != num_map.end()) {
+                return {num_map[complement], i};
+            }
+            
+            num_map[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
+*/
