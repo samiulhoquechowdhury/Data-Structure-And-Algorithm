a19974f07a91349a106f2a0f00f6ac5008a0866e
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-03 02:29:53 +0530

    example

---
+++ Array/example.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[]= {12,324,12,54,26};
+    cout<<"The size of arr is : " << sizeof(arr)<<endl;
+    cout<<"The size of arr is : " << sizeof(arr[0])<<endl;
+    
+    return 0;
+}
