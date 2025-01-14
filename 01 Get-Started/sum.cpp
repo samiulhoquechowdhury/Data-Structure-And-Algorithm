ea71fdc124ae0624e6087e66577ce2fb92c0f442
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-14 13:06:13 +0530

    sum of two number

---
+++ 01 Get-Started/sum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 23;
+    int b = 12;
+    int sum = a + b;
+
+    cout<<"The sum of A and B is :"<< sum << endl;
+    return 0;
+}
