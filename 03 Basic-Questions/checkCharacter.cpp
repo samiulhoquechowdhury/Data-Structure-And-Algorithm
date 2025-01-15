b6e42639612f0114647fecd220bdeeaaec41905e
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-15 22:20:18 +0530

    checking  charater.

---
+++ 03 Basic-Questions/checkCharacter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char ch;
+    cout<< "Enter a character :"<<endl;
+    cin>> ch;
+
+    //Alphabet checking condition
+
+    if ((ch >= 'a' && ch <='z') || (ch >='A' && ch <= 'Z'))
+    {
+        cout<< ch << " is an Alphabet" << endl;
+    } else if(ch  > '0' && ch <= '9'){
+        cout<< ch << " is a Digit" << endl;
+    } else {
+        cout<< ch << " is a special character" << endl;
+    }
+    
+
+    return 0;
+}
