7a9e8a6302c1b2d34f2be2f93b9a60bf2aa9d89d
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-16 01:21:09 +0530

    reverse a string.

---
+++ 03 Basic-Questions/reverseString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring> // Corrected header for strlen()
+
+using namespace std;
+
+int main() {
+    int i, j, length;
+    char string[100]; // Removed unnecessary semicolon in #define MAX_SIZE
+    char reverse[100];
+
+    cout << "Enter a string: ";
+    cin >> string; // Use cin to take input
+
+    length = strlen(string); // Get the length of the input string
+    j = 0;
+
+    // Loop to reverse the string
+    for (i = length - 1; i >= 0; i--) {
+        reverse[j] = string[i];
+        j++;
+    }
+    reverse[j] = '\0'; // Null-terminate the reversed string
+
+    cout << "Reversed string: " << reverse << endl;
+
+    return 0;
+}
