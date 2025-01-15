679948f5c1d7e5f48156e65073c4660bc38d3df8
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-01-15 20:00:48 +0530

    updataion of varibale

---
+++ 01 Get-Started/SizeofFundamentalDatatype.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "The size of char is :" << sizeof(char) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(short) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(int) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(long) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(long long) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(float) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(double) << "bytes \n" ;
+    cout << "The size of char is :" << sizeof(long double) << "bytes \n" ;
+    return 0;
+}
